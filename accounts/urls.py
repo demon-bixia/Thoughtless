199a83bfd7a666205b91e4cf9b3c2751dd2ef784
mohammad salah ali <MsMainAcc0unt@gmail.com>
2019-12-23 23:07:38 +0200

    fixed email activation errors

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path("register/", auth_views.RegisterView.as_view(), name="register-view"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout-view"),
     path("valid_email/", auth_views.AjaxRegister.as_view(), name="validate-email"),
-    re_path(r"^active/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-            auth_views.ActivateAccount.as_view(), name="active"),
-    # re_path(r"^active/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
-            # auth_views.ActivateAccount.as_view(), name="active"),
+
+    # account activate
+    path('active/<uidb64>/<token>/', auth_views.ActivateAccount.as_view(), name="active"),
+
     # password_reset
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
