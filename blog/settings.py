3f88cf0601a93d6dcd97d0eccb71ed7e4194e1f2
demon-bixia <msmainacc0unt@gmail.com>
2023-03-30 08:14:35 +0200

    fix database engine

--- blog/settings.py
+++ blog/settings.py
@@ -60,11 +60,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DATABASE_NAME'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'USER': os.environ.get('DATABASE_USER'),
-        'HOST': os.environ.get('DATABASE_PORT'),
+        'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
